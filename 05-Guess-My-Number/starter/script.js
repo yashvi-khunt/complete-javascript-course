96e53a439c169ef1b24130873b0477e51e233283
yashvi-khunt <yashvi@weybee.in>
2023-08-08 18:14:39 +0530

    Did hands on after coding challenge 1. COntinue with the pig game hold event from next time

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,42 +4,39 @@ let secretNumber = Math.trunc(Math.random() * 20)+1;
 let score = 20
 let highscore = 0;
 
+const displayMessage = (message) => document.querySelector('.message').textContent = message;
+
+
 document.querySelector('.check').addEventListener('click', ()=>{
+    
     const guess = Number(document.querySelector('.guess').value);
+    
     if(!guess){
-        document.querySelector('.message').textContent = 'No Number!';
-    } else if(guess === secretNumber){
+        displayMessage('No Number!');
+    }else if(guess === secretNumber){
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
-        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        displayMessage('🎉 Correct Number!');
 
         if(score>highscore){
             highscore = score
             document.querySelector('.highscore').textContent = highscore;
         }
 
-    } else if (guess > secretNumber){
+    }else if(guess !== secretNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = 'Too high!';
+            displayMessage(guess > secretNumber ? 'Too high!': 'Too low!');
             score--;
             document.querySelector('.score').textContent = score
         }
         else{
-            document.querySelector('.message').textContent = 'you lost the game';
+            displayMessage('you lost the game');
             document.querySelector('.score').textContent = 0
         }
-        
-    } else if (guess < secretNumber){
-        if(score > 1){
-            document.querySelector('.message').textContent = 'Too low!';
-            score--;
-            document.querySelector('.score').textContent = score
-        }
-        else{
-            document.querySelector('.message').textContent = 'you lost the game';
-            document.querySelector('.score').textContent = 0
-        }
+    }else{
+        displayMessage('you lost the game');
+        document.querySelector('.score').textContent = 0;
     }
 });
 
@@ -47,7 +44,7 @@ document.querySelector('.check').addEventListener('click', ()=>{
 document.querySelector('.again').addEventListener('click', ()=>{
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20)+1;
-    document.querySelector('.message').textContent = 'Start guessing...'
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score
     document.querySelector('.number').textContent = '?';
     document.querySelector('body').style.backgroundColor = '#222';
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+//selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currentEl0 = document.getElementById('current--0');
+const currentEl1 = document.getElementById('current--1');
+
+const scores = [0,0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//initial view
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEL.classList.add('hidden');
+
+
+//rolling dice
+btnRoll.addEventListener('click', ()=>{
+    //1. Generating a random dice roll
+    const diceValue = Math.trunc(Math.random()*6)+1;
+    
+    //2. Display dice
+    diceEL.classList.remove('hidden');
+    diceEL.src = `dice-${diceValue}.png`;
+
+    //3. Check for 1
+    if(diceValue !== 1){
+        //Add dice value to current score
+        currentScore += diceValue;
+
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+    }
+    else{
+        //current player reset & bg change
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+     
+        //switch player
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+        
+    }
+})
+
+btnHold.addEventListener('click',()=>{
+    //1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
+    //check if player's score is >= 100
+    //finish game
+
+
+    //Switch to the next player
+})
+
